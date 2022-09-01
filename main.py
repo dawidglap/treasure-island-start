d8591e8a1007b6391d5bea906061a6891379869e
Dawid Glapiak <81537977+dawidglap@users.noreply.github.com>
2022-09-01 14:07:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+choice_1 = input("do you want to go 'left' o 'right'?")
+if choice_1 == 'left':
+  choice_2 = input("You reach a beach. do you want to 'swim' or 'wait' for the boat?")
+  if choice_2 == 'wait':
+    choice_3 = input("You reach a home with 3 doors. yellow, red, and blue. Which one do you want to open?")
+    if choice_3 == 'yellow':
+      print("You found the treasure YOU WIN!")
+    else:
+      print("Behind the door you met your worst enemy. He killed you. GAME OVER")
+  elif choice_2 == 'swim':
+      print("GAME OVER, you forget that you are not able to swim.")
+elif choice_1 == 'right':
+  print("You fell into a black hole, GAME OVER")
